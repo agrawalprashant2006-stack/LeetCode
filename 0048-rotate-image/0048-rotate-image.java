3ccf707abbb25f901ff7c8fd5f8745227e46c11c
agrawalprashant2006-stack <agrawalprashant2006@gmail.com>
2026-09-16 15:47:40 +0530

    Time: 0 ms (100%), Space: 43.5 MB (85.59%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        Transpose(matrix);
+        Rotate(matrix);
+    }
+    public static void Transpose(int arr[][]){
+        for(int i = 0; i<arr.length; i++){
+            for(int j = i+1; j<arr[0].length; j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+    }
+    public static void Rotate(int arr[][]){
+        for(int i = 0; i<arr.length; i++){
+            int left = 0;
+            int right = arr.length-1;
+            while(left<right){
+                int temp = arr[i][left];
+                arr[i][left] = arr[i][right];
+                arr[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
