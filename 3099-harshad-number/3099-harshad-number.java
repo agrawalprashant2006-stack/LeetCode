14209fb2aa4ee0bdbacbedf0ccbfbbe3d430c200
agrawalprashant2006-stack <agrawalprashant2006@gmail.com>
2026-09-16 11:24:12 +0530

    Time: 0 ms (100%), Space: 42.3 MB (25.22%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0;
+        int original = x;
+        while(x!=0){
+            int d = x%10;
+            sum+=d;
+            x/=10;
+        }
+        if(original%sum == 0){
+            return sum;
+        }
+        return -1;
+    }
+}
